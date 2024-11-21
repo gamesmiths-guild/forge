f083b916ea7b420df6dea11917c963e6d13f3b7a
Lex <lex@lextatic.com>
2024-11-20 23:13:24 -0300

    Added GameplayTagsManagerFixture

--- Forge.Tests/GameplayTags/GameplayTagTests.cs
+++ Forge.Tests/GameplayTags/GameplayTagTests.cs
@@ -6,42 +6,9 @@ using Gamesmiths.Forge.GameplayTags;
 
 namespace Gamesmiths.Forge.Tests.GameplayTags;
 
-public class GameplayTagTests
+public class GameplayTagTests(GameplayTagsManagerFixture fixture) : IClassFixture<GameplayTagsManagerFixture>
 {
-	private readonly GameplayTagsManager _gameplayTagsManager;
-
-	public GameplayTagTests()
-	{
-		_gameplayTagsManager = new GameplayTagsManager([
-			"tag",
-			"simple",
-			"simple.tag",
-			"other.tag",
-			"enemy.undead.zombie",
-			"enemy.undead.skeleton",
-			"enemy.undead.ghoul",
-			"enemy.undead.vampire",
-			"enemy.beast.wolf",
-			"enemy.beast.wolf.gray",
-			"enemy.beast.boar",
-			"enemy.humanoid.goblin",
-			"enemy.humanoid.orc",
-			"item.consumable.potion.health",
-			"item.consumable.potion.mana",
-			"item.consumable.potion.stamina",
-			"item.consumable.food.apple",
-			"item.consumable.food.bread",
-			"item.equipment.weapon.sword",
-			"item.equipment.weapon.dagger",
-			"item.equipment.weapon.axe",
-			"color.red",
-			"color.green",
-			"color.blue",
-			"color.dark.red",
-			"color.dark.green",
-			"color.dark.blue"
-		]);
-	}
+	private readonly GameplayTagsManager _gameplayTagsManager = fixture.GameplayTagsManager;
 
 	[Theory]
 	[Trait("Request", "Registered tags")]
