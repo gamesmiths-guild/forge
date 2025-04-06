f506771266955eda2f681db3376ccb2518cea023
Lex <lex@lextatic.com>
2025-04-06 17:50:16 -0300

    Make QueryExpressionType enum public

--- Forge/GameplayTags/GameplayTagQueryExpressionType.cs
+++ Forge/GameplayTags/GameplayTagQueryExpressionType.cs
@@ -1,20 +1,59 @@
-// Copyright © 2024 Gamesmiths Guild.
+// Copyright © 2025 Gamesmiths Guild.
 
 namespace Gamesmiths.Forge.GameplayTags;
 
 /// <summary>
 /// Types of <see cref="GameplayTagQueryExpression"/>s.
 /// </summary>
-internal enum GameplayTagQueryExpressionType
+public enum GameplayTagQueryExpressionType
 {
+	/// <summary>
+	/// Not defined.
+	/// </summary>
 	Undefined = 0,
+
+	/// <summary>
+	/// Any tags must match.
+	/// </summary>
 	AnyTagsMatch = 1,
+
+	/// <summary>
+	/// All tags must match.
+	/// </summary>
 	AllTagsMatch = 2,
+
+	/// <summary>
+	/// No tags must match.
+	/// </summary>
 	NoTagsMatch = 3,
+
+	/// <summary>
+	/// Any tags must match exactly.
+	/// </summary>
 	AnyTagsMatchExact = 4,
+
+	/// <summary>
+	/// All tags must match exactly.
+	/// </summary>
 	AllTagsMatchExact = 5,
+
+	/// <summary>
+	/// No tags must match exactly.
+	/// </summary>
 	NoTagsMatchExact = 6,
+
+	/// <summary>
+	/// Any expressions must match.
+	/// </summary>
 	AnyExpressionsMatch = 7,
+
+	/// <summary>
+	/// All expressions must match.
+	/// </summary>
 	AllExpressionsMatch = 8,
+
+	/// <summary>
+	/// No expressions must match.
+	/// </summary>
 	NoExpressionsMatch = 9,
 }
