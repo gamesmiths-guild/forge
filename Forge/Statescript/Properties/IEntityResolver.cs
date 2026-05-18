da051836e7d72004067a8c4ff7afaad6ff1de30c
Lex <lex@lextatic.com>
2026-05-17 22:05:48 -0300

    Removed unnecessary brackets

--- Forge/Statescript/Properties/IEntityResolver.cs
+++ Forge/Statescript/Properties/IEntityResolver.cs
@@ -7,6 +7,4 @@ namespace Gamesmiths.Forge.Statescript.Properties;
 /// <summary>
 /// Resolves an <see cref="IForgeEntity"/> reference at runtime.
 /// </summary>
-public interface IEntityResolver : IReferenceResolver<IForgeEntity>
-{
-}
+public interface IEntityResolver : IReferenceResolver<IForgeEntity>;
