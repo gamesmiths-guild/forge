bd0c6538018171d1ea29cc6e345d9584b46999e9
Lex <lex@lextatic.com>
2026-06-13 23:15:52 -0300

    Added Cue nodes tests

---
+++ Forge.Tests/Statescript/Nodes/Action/ExecuteCueNodeTests.cs
@@ -0,0 +1,141 @@
+// Copyright © Gamesmiths Guild.
+
+using FluentAssertions;
+using Gamesmiths.Forge.Core;
+using Gamesmiths.Forge.Cues;
+using Gamesmiths.Forge.Statescript;
+using Gamesmiths.Forge.Statescript.Nodes;
+using Gamesmiths.Forge.Statescript.Nodes.Action;
+using Gamesmiths.Forge.Tags;
+using Gamesmiths.Forge.Tests.Helpers;
+
+using static Gamesmiths.Forge.Tests.Helpers.NodeBindings;
+
+namespace Gamesmiths.Forge.Tests.Statescript.Nodes.Action;
+
+public class ExecuteCueNodeTests(TagsAndCuesFixture tagsAndCuesFixture) : IClassFixture<TagsAndCuesFixture>
+{
+	private readonly TagsManager _tagsManager = tagsAndCuesFixture.TagsManager;
+
+	[Theory]
+	[Trait("Graph", "ExecuteCue")]
+	[InlineData(false, false)]
+	[InlineData(false, true)]
+	[InlineData(true, false)]
+	[InlineData(true, true)]
+	public void Execute_cue_node_supports_all_scalar_and_array_combinations(bool useTagArray, bool useTargetArray)
+	{
+		var cuesManager = new CuesManager();
+		var firstHandler = new RecordingCueHandler();
+		var secondHandler = new RecordingCueHandler();
+		var firstCue = Tag.RequestTag(_tagsManager, "test.cue1");
+		var secondCue = Tag.RequestTag(_tagsManager, "test.cue2");
+		cuesManager.RegisterCue(firstCue, firstHandler);
+		cuesManager.RegisterCue(secondCue, secondHandler);
+
+		var primaryTarget = new TestEntity(_tagsManager, cuesManager);
+		var secondaryTarget = new TestEntity(_tagsManager, cuesManager);
+
+		var graph = new Graph();
+		ConfigureCueTagInput(graph, useTagArray, firstCue, secondCue);
+		ConfigureTargetInput(graph, useTargetArray, primaryTarget, secondaryTarget);
+		graph.VariableDefinitions.DefineVariable("magnitude", 7);
+
+		ExecuteCueNode node = CreateExecuteCueNode("cueTag", "target");
+		node.BindInput(ExecuteCueNode.MagnitudeInput, "magnitude");
+		graph.AddNode(node);
+		graph.AddConnection(new Connection(
+			graph.EntryNode.OutputPorts[EntryNode.OutputPort],
+			node.InputPorts[ActionNode.InputPort]));
+
+		var processor = new GraphProcessor(graph);
+		processor.StartGraph();
+
+		int expectedPerTag = useTargetArray ? 2 : 1;
+		firstHandler.ExecuteCount.Should().Be(expectedPerTag);
+		secondHandler.ExecuteCount.Should().Be(useTagArray ? expectedPerTag : 0);
+	}
+
+	[Fact]
+	[Trait("Graph", "ExecuteCue")]
+	public void Execute_cue_node_passes_resolved_parameters_to_the_handler()
+	{
+		var cuesManager = new CuesManager();
+		var handler = new RecordingCueHandler();
+		var cue = Tag.RequestTag(_tagsManager, "test.cue1");
+		cuesManager.RegisterCue(cue, handler);
+		var target = new TestEntity(_tagsManager, cuesManager);
+
+		var graph = new Graph();
+		graph.VariableDefinitions.DefineObjectVariable("cueTag", cue);
+		graph.VariableDefinitions.DefineObjectVariable<IForgeEntity>("target", target);
+		graph.VariableDefinitions.DefineVariable("magnitude", 12);
+		graph.VariableDefinitions.DefineVariable("normalized", 0.75f);
+
+		ExecuteCueNode node = CreateExecuteCueNode("cueTag", "target");
+		node.BindInput(ExecuteCueNode.MagnitudeInput, "magnitude");
+		node.BindInput(ExecuteCueNode.NormalizedMagnitudeInput, "normalized");
+		graph.AddNode(node);
+		graph.AddConnection(new Connection(
+			graph.EntryNode.OutputPorts[EntryNode.OutputPort],
+			node.InputPorts[ActionNode.InputPort]));
+
+		var processor = new GraphProcessor(graph);
+		processor.StartGraph();
+
+		handler.ExecuteCount.Should().Be(1);
+		handler.LastTarget.Should().BeSameAs(target);
+		handler.LastParameters.Should().NotBeNull();
+		handler.LastParameters!.Value.Magnitude.Should().Be(12);
+		handler.LastParameters.Value.NormalizedMagnitude.Should().Be(0.75f);
+	}
+
+	[Fact]
+	[Trait("Graph", "ExecuteCue")]
+	public void Execute_cue_node_passes_null_parameters_when_no_parameter_inputs_are_bound()
+	{
+		var cuesManager = new CuesManager();
+		var handler = new RecordingCueHandler();
+		var cue = Tag.RequestTag(_tagsManager, "test.cue1");
+		cuesManager.RegisterCue(cue, handler);
+		var target = new TestEntity(_tagsManager, cuesManager);
+
+		var graph = new Graph();
+		graph.VariableDefinitions.DefineObjectVariable("cueTag", cue);
+		graph.VariableDefinitions.DefineObjectVariable<IForgeEntity>("target", target);
+
+		ExecuteCueNode node = CreateExecuteCueNode("cueTag", "target");
+		graph.AddNode(node);
+		graph.AddConnection(new Connection(
+			graph.EntryNode.OutputPorts[EntryNode.OutputPort],
+			node.InputPorts[ActionNode.InputPort]));
+
+		var processor = new GraphProcessor(graph);
+		processor.StartGraph();
+
+		handler.ExecuteCount.Should().Be(1);
+		handler.LastParameters.Should().BeNull();
+	}
+
+	private static void ConfigureCueTagInput(Graph graph, bool useArray, Tag firstCue, Tag secondCue)
+	{
+		if (useArray)
+		{
+			graph.VariableDefinitions.DefineObjectArrayVariable("cueTag", firstCue, secondCue);
+			return;
+		}
+
+		graph.VariableDefinitions.DefineObjectVariable("cueTag", firstCue);
+	}
+
+	private static void ConfigureTargetInput(Graph graph, bool useArray, TestEntity primary, TestEntity secondary)
+	{
+		if (useArray)
+		{
+			graph.VariableDefinitions.DefineObjectArrayVariable<IForgeEntity>("target", primary, secondary);
+			return;
+		}
+
+		graph.VariableDefinitions.DefineObjectVariable<IForgeEntity>("target", primary);
+	}
+}
---
+++ Forge.Tests/Statescript/Nodes/State/CueNodeTests.cs
@@ -0,0 +1,134 @@
+// Copyright © Gamesmiths Guild.
+
+using FluentAssertions;
+using Gamesmiths.Forge.Core;
+using Gamesmiths.Forge.Cues;
+using Gamesmiths.Forge.Statescript;
+using Gamesmiths.Forge.Statescript.Nodes;
+using Gamesmiths.Forge.Statescript.Nodes.State;
+using Gamesmiths.Forge.Tags;
+using Gamesmiths.Forge.Tests.Helpers;
+
+using static Gamesmiths.Forge.Tests.Helpers.NodeBindings;
+
+namespace Gamesmiths.Forge.Tests.Statescript.Nodes.State;
+
+public class CueNodeTests(TagsAndCuesFixture tagsAndCuesFixture) : IClassFixture<TagsAndCuesFixture>
+{
+	private readonly TagsManager _tagsManager = tagsAndCuesFixture.TagsManager;
+
+	[Theory]
+	[Trait("Graph", "CueNode")]
+	[InlineData(false, false)]
+	[InlineData(false, true)]
+	[InlineData(true, false)]
+	[InlineData(true, true)]
+	public void Cue_node_applies_all_scalar_and_array_combinations_on_activation(bool useTagArray, bool useTargetArray)
+	{
+		var cuesManager = new CuesManager();
+		var firstHandler = new RecordingCueHandler();
+		var secondHandler = new RecordingCueHandler();
+		var firstCue = Tag.RequestTag(_tagsManager, "test.cue1");
+		var secondCue = Tag.RequestTag(_tagsManager, "test.cue2");
+		cuesManager.RegisterCue(firstCue, firstHandler);
+		cuesManager.RegisterCue(secondCue, secondHandler);
+
+		var primaryTarget = new TestEntity(_tagsManager, cuesManager);
+		var secondaryTarget = new TestEntity(_tagsManager, cuesManager);
+
+		var graph = new Graph();
+
+		if (useTagArray)
+		{
+			graph.VariableDefinitions.DefineObjectArrayVariable("cueTag", firstCue, secondCue);
+		}
+		else
+		{
+			graph.VariableDefinitions.DefineObjectVariable("cueTag", firstCue);
+		}
+
+		if (useTargetArray)
+		{
+			graph.VariableDefinitions.DefineObjectArrayVariable<IForgeEntity>("target", primaryTarget, secondaryTarget);
+		}
+		else
+		{
+			graph.VariableDefinitions.DefineObjectVariable<IForgeEntity>("target", primaryTarget);
+		}
+
+		CueNode node = CreateCueNode("cueTag", "target");
+		graph.AddNode(node);
+		graph.AddConnection(new Connection(
+			graph.EntryNode.OutputPorts[EntryNode.OutputPort],
+			node.InputPorts[StateNode<CueNodeContext>.InputPort]));
+
+		var processor = new GraphProcessor(graph);
+		processor.StartGraph();
+
+		int expectedPerTag = useTargetArray ? 2 : 1;
+		firstHandler.ApplyCount.Should().Be(expectedPerTag);
+		secondHandler.ApplyCount.Should().Be(useTagArray ? expectedPerTag : 0);
+		firstHandler.IsApplied.Should().BeTrue();
+	}
+
+	[Fact]
+	[Trait("Graph", "CueNode")]
+	public void Cue_node_removes_applied_cues_on_deactivation_without_interruption_by_default()
+	{
+		(GraphProcessor processor, RecordingCueHandler handler) = BuildSingleCueGraph();
+
+		processor.StartGraph();
+		handler.ApplyCount.Should().Be(1);
+		handler.IsApplied.Should().BeTrue();
+
+		processor.StopGraph();
+
+		handler.RemoveCount.Should().Be(1);
+		handler.IsApplied.Should().BeFalse();
+		handler.LastInterrupted.Should().BeFalse();
+	}
+
+	[Fact]
+	[Trait("Graph", "CueNode")]
+	public void Cue_node_marks_removal_interrupted_when_deactivated_through_the_abort_port()
+	{
+		// Aborting on start fires after activation (synchronous fan-out in connection order), so the cues are applied
+		// and then removed as an interruption in the same StartGraph call.
+		(GraphProcessor processor, RecordingCueHandler handler) = BuildSingleCueGraph(abortOnStart: true);
+
+		processor.StartGraph();
+
+		handler.ApplyCount.Should().Be(1);
+		handler.RemoveCount.Should().Be(1);
+		handler.IsApplied.Should().BeFalse();
+		handler.LastInterrupted.Should().BeTrue();
+	}
+
+	private (GraphProcessor Processor, RecordingCueHandler Handler) BuildSingleCueGraph(bool abortOnStart = false)
+	{
+		var cuesManager = new CuesManager();
+		var handler = new RecordingCueHandler();
+		var cue = Tag.RequestTag(_tagsManager, "test.cue1");
+		cuesManager.RegisterCue(cue, handler);
+		var target = new TestEntity(_tagsManager, cuesManager);
+
+		var graph = new Graph();
+		graph.VariableDefinitions.DefineObjectVariable("cueTag", cue);
+		graph.VariableDefinitions.DefineObjectVariable<IForgeEntity>("target", target);
+
+		CueNode node = CreateCueNode("cueTag", "target");
+		graph.AddNode(node);
+		graph.AddConnection(new Connection(
+			graph.EntryNode.OutputPorts[EntryNode.OutputPort],
+			node.InputPorts[StateNode<CueNodeContext>.InputPort]));
+
+		if (abortOnStart)
+		{
+			graph.AddConnection(new Connection(
+				graph.EntryNode.OutputPorts[EntryNode.OutputPort],
+				node.InputPorts[StateNode<CueNodeContext>.AbortPort]));
+		}
+
+		return (new GraphProcessor(graph), handler);
+	}
+}
