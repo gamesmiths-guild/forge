c1ebd1d7fac992817241949d0064f3bdd24f9b00
Lex <lex@lextatic.com>
2026-08-03 13:23:43 -0300

    Added attribute quantization tests

---
+++ Forge.Tests/Attributes/AttributeDecimalPlacesValidationTests.cs
@@ -0,0 +1,55 @@
+// Copyright © Gamesmiths Guild.
+
+using FluentAssertions;
+using Gamesmiths.Forge.Attributes;
+using Gamesmiths.Forge.Core;
+
+namespace Gamesmiths.Forge.Tests.Attributes;
+
+public sealed class AttributeDecimalPlacesValidationTests : IDisposable
+{
+	public AttributeDecimalPlacesValidationTests()
+	{
+		Validation.Enabled = true;
+	}
+
+	public void Dispose()
+	{
+		Validation.Enabled = false;
+		GC.SuppressFinalize(this);
+	}
+
+	[Theory]
+	[Trait("Decimal places", null)]
+	[InlineData(0)]
+	[InlineData(1)]
+	[InlineData(9)]
+	public void A_scale_that_fits_in_an_int_is_accepted(int decimalPlaces)
+	{
+		Action act = () => _ = new ConfigurableAttributeSet(decimalPlaces);
+
+		act.Should().NotThrow();
+	}
+
+	[Theory]
+	[Trait("Decimal places", null)]
+	[InlineData(-1)]
+	[InlineData(10)]
+	public void A_scale_that_does_not_fit_in_an_int_is_rejected(int decimalPlaces)
+	{
+		// Ten places would need a scale of 10^10, which overflows the int every attribute value is stored in.
+		Action act = () => _ = new ConfigurableAttributeSet(decimalPlaces);
+
+		act.Should().Throw<ValidationException>();
+	}
+
+	private sealed class ConfigurableAttributeSet : AttributeSet
+	{
+		public EntityAttribute Scaled { get; }
+
+		public ConfigurableAttributeSet(int decimalPlaces)
+		{
+			Scaled = InitializeAttribute(nameof(Scaled), 0, 0, int.MaxValue, decimalPlaces: decimalPlaces);
+		}
+	}
+}
